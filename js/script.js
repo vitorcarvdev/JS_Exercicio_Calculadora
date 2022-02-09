e6dd2f59d8e846d05a1c23ef6f9a17f1a3258f06
Vitor Carv <94801330+vitorcarvdev@users.noreply.github.com>
2022-02-09 16:54:40 -0200

    Refazendo sistema

--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
 
 var infotab = document.querySelector('#resultado');
 
-var numb = prompt('Digite um numero');
-numb = parseFloat(numb);
+//var numb = prompt('Digite um numero');
+//numb = parseFloat(numb);
 
 function calcular(simbulo){
-    var numbDois = prompt('Digite o segundo numero');
-    numbDois = parseFloat(numbDois);
-
-    switch(simbulo){
-        case '+':
-        infotab.innerHTML = numb + numbDois;
-        break;
-        case '-':
-        infotab.innerHTML = numb - numbDois;
-        break;
-        case '*':
-        infotab.innerHTML = numb * numbDois;
-        break;
-        case '/':
-        infotab.innerHTML = numb / numbDois;
-        break;
-        default: alert("Algo deu errado");
-    }        
+    //var numbDois = prompt('Digite o segundo numero');
+    //numbDois = parseFloat(numbDois);      
 }    
